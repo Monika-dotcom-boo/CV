f810dcfc0eaf5ca723386810be591425cded02e1
Monika Rasała <mrasala583@gmail.com>
2024-01-12 16:20:59 +0100

    giving an outline

---
+++ CV/src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+
+  return (
+    <div>
+      <div>
+       <section>Navbar</section>
+       <section>Parallax</section>
+       <section>Services</section>
+       <section>Parallax</section>
+       <section>Portfolio 1</section>
+       <section>Portfolio 2</section>
+       <section>Portfolio 3</section>
+       <section>Contact</section>
+     </div> 
+    </div>  
+  )
+}
+
+export default App
+
---
+++ CV/src/componets/navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return (
+        <div>
+            <p>Navbart</p>
+            <div>
+                <div>
+                    <span>Monika Rasała</span>
+                    <div>
+                        <p>Social</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
