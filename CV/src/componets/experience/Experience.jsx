cbe44f6a50957b67d5e57cfcf6dbcbf1f38057d8
Monika Rasała <mrasala583@gmail.com>
2024-01-22 12:05:46 +0100

    access to the website's content database

--- CV/src/componets/experience/Experience.jsx
+++ CV/src/componets/experience/Experience.jsx
@@ -2,9 +2,24 @@
 function Experience() {
     return (
       <div>
-        
-        <section />
-        
+        <div>
+          <p>360 Software House</p>
+          {/* //Link do strony: https://360softwarehouse.pl/ */}
+          <p>
+            During the internship and then as a Junior Frontend Developer writing in React,
+            {' '}
+            <br />
+            I learned, among others: close cooperation between all departments working on the project.  
+            {' '}
+            <br />
+            As the only person responsible for the front end of the web application, 
+            {' '}
+            <br />
+            I had the opportunity to learn discipline in the full sense of the word.
+
+          </p>
+
+        </div>
       </div>
     );
   }
--- CV/src/componets/skille/Skille.jsx
+++ CV/src/componets/skille/Skille.jsx
@@ -1,27 +1,35 @@
 
 function Skille() {
     return (
-      <div className="text-white bg-black">
+      <div>
+        
         <div>
-          <div>
-            <h4>Web development</h4>
-          </div>
-          <div>
-            <p>HTML 5</p>
-            <p>SCSS</p>
-            <p>CSS</p>
-            <p>JavaScript</p>
-            <p>Vue</p>
-            <p>Bootstrap</p>
-            <p>Tailwind</p>
-            <p>Git</p>
-          </div>
+          <p className="underline-offset-2">Web development</p>
+        </div>
+        <div>
+          <p>HTML 5</p>
+          <p>SCSS</p>
+          <p>CSS</p>
+          <p>JavaScript</p>
+          <p>Vue</p>
+          <p>Bootstrap</p>
+          <p>Tailwind</p>
+          <p>Git</p>
+          <p>Docker</p>
+          <p>MUI</p>
         </div>
         <div>
           <h4>Design</h4>
           <p>Figma</p>
           <p>Adobe XD</p>
+          <p>Canvas</p>
         </div>
+        <div>
+          <p>Project management</p>
+          <p>Jira</p>
+          <p>Slack</p>
+        </div>
+        
       </div>
     );
   }
