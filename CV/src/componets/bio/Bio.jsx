4cc417ccca5c7096fb0e061c95af4c0ec1136dec
Monika Rasała <mrasala583@gmail.com>
2024-01-22 15:25:16 +0100

    remove bio

--- CV/src/componets/bio/Bio.jsx
+++ CV/src/componets/bio/Bio.jsx
@@ -3,7 +3,19 @@ function Bio() {
     return (
       <div>
         <div>
-          ...
+          <p>Passionate learner exploring web development</p>
+        </div>
+        <div>
+          <p>
+            My Ambition
+            After work, I enthusiastically pursue self-learning in front-end development.
+            {' '}
+            <br />
+            Despite not working in IT professionally, my evenings are dedicated to expanding 
+            {' '}
+            <br />
+            my knowledge and honing web development skills.
+          </p>
         </div>
       </div>
     )
--- CV/src/componets/layout/Header.jsx
+++ CV/src/componets/layout/Header.jsx
@@ -15,9 +15,9 @@ function Header() {
                 <li>
                   <a href="/Home" className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white" aria-current="page">Home</a>
                 </li>
-                <li>
+                {/* <li>
                   <a href="/Bio" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Bio</a>
-                </li>
+                </li> */}
                 <li>
                   <a href="/About" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">About</a>
                 </li>
