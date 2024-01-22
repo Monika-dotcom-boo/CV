1d315236254f9c114454827eef1e2a0ce546ac5a
Monika Rasała <mrasala583@gmail.com>
2024-01-22 10:09:41 +0100

    complement to the kills page

--- CV/src/componets/skille/Skille.jsx
+++ CV/src/componets/skille/Skille.jsx
@@ -1,10 +1,27 @@
 
 function Skille() {
     return (
-      <div className="bg-black">
-        
-        <section />
-        
+      <div>
+        <div>
+          <div>
+            <h4>Web development</h4>
+          </div>
+          <div>
+            <p>HTML 5</p>
+            <p>SCSS</p>
+            <p>CSS</p>
+            <p>JavaScript</p>
+            <p>Vue</p>
+            <p>Bootstrap</p>
+            <p>Tailwind</p>
+            <p>Git</p>
+          </div>
+        </div>
+        <div>
+          <h4>Design</h4>
+          <p>Figma</p>
+          <p>Adobe XD</p>
+        </div>
       </div>
     );
   }
