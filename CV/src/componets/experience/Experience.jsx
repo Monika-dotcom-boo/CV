92f2635f2aabf7b9b65911e418e269b7acde7c90
Monika Rasała <mrasala583@gmail.com>
2024-02-02 10:14:43 +0100

    improvement experience

--- CV/src/componets/experience/Experience.jsx
+++ CV/src/componets/experience/Experience.jsx
@@ -2,10 +2,21 @@
 function Experience() {
     return (
       <div>
-        <div>
-          <p>360 Software House</p>
-          {/* //Link do strony: https://360softwarehouse.pl/ */}
-          <p>
+        <div className="text-white ml-10">
+        <a href="https://expertwww.pl/" className="text-bold text-2xl mt-7">ExpertWWW Piotr Gruszecki</a>
+        <p>Duration of the agreement: 02.2024 - </p>
+        <p className="mt-7 mb-7">
+         At the company, I continued to learn the secrets of working with Figma 
+         {' '}
+          <br />
+         and had the opportunity to learn how to create websites on WordPress
+          </p>
+        </div>
+
+        <div className="text-white ml-10 mt-14">
+        <a href="https://360softwarehouse.pl/" className="text-bold text-2xl ">360 Software House</a>
+        <p>Duration of the agreement: 09.2023 - </p>
+          <p className="mt-7 mb-7">
             During the internship and then as a Junior Frontend Developer writing in React,
             {' '}
             <br />
@@ -16,9 +27,7 @@ function Experience() {
             {' '}
             <br />
             I had the opportunity to learn discipline in the full sense of the word.
-
           </p>
-
         </div>
       </div>
     );
